87afa48f07006edb7558e3f926eaf1e2b58acf9c
DomenikoD <domeniko.divjak@gmail.com>
2019-02-23 23:01:25 +0100

    asdf

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import BaseIcon from "@/components/BaseIcon.vue";
+
+Vue.component("BaseIcon", BaseIcon);
 
 Vue.config.productionTip = false;
 
